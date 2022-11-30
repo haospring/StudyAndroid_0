21d92ff21e852b926a69868bf43464bfbcb60d22
haospring <haospring123@gmail.com>
2022-11-30 21:51:55 +0800

    update launch mode

--- chapter_1/testlaunchmode/src/main/java/com/hcs/testlaunchmode/FirstActivity.java
+++ chapter_1/testlaunchmode/src/main/java/com/hcs/testlaunchmode/FirstActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,15 +24,26 @@ public class FirstActivity extends AppCompatActivity implements View.OnClickList
                 BitmapFactory.decodeResource(getResources(), R.drawable.calendar)));
 
         mBinding.btnFirst.setOnClickListener(this);
+        mBinding.btnFirst2.setOnClickListener(this);
     }
 
 
     @Override
     public void onClick(View v) {
+        int viewId = v.getId();
         Intent intent = new Intent();
-        intent.setAction("com.hcs.testlaunchmode.SecondActivity");
-        if (getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY) != null) {
-            startActivity(intent);
+        if (viewId == mBinding.btnFirst.getId()) {
+            intent.setAction("com.hcs.testlaunchmode.SecondActivity");
+
+            if (getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY) != null) {
+                startActivity(intent);
+            }
+        } else if (viewId == mBinding.btnFirst2.getId()) {
+            intent.setAction("com.hcs.testconfigchanges.SecondActivity");
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                Log.d("haospring3", "onClick: ");
+                startActivity(intent);
+            }
         }
     }
 }
