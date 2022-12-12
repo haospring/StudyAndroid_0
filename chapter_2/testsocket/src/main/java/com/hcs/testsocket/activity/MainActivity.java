ff03e0edb1dca0980df2b8a41fff2d676d7409a4
ZKCD-haocs <zkcd-haocs@dfmc.com.cn>
2022-12-12 18:09:15 +0800

    update test socket

--- chapter_2/testsocket/src/main/java/com/hcs/testsocket/activity/MainActivity.java
+++ chapter_2/testsocket/src/main/java/com/hcs/testsocket/activity/MainActivity.java
@@ -4,22 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.hcs.testsocket.R;
-import com.hcs.testsocket.utils.LogUtils;
-
-import java.util.ArrayList;
-
-import timber.log.Timber;
+import com.hcs.testsocket.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
+    private ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        LogUtils.logV(this, "");
-        LogUtils.logV(MainActivity.class, "test %s", "haospring");
-        LogUtils.logV(TAG, "test %d", 1);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
     }
 }
