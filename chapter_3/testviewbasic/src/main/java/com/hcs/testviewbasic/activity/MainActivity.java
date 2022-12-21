0775e35d54bb9963a61ec70c973a1c2dcca5c5c3
ZKCD-haocs <zkcd-haocs@dfmc.com.cn>
2022-12-21 17:47:55 +0800

    update test view basic

--- chapter_3/testviewbasic/src/main/java/com/hcs/testviewbasic/activity/MainActivity.java
+++ chapter_3/testviewbasic/src/main/java/com/hcs/testviewbasic/activity/MainActivity.java
@@ -3,14 +3,33 @@ package com.hcs.testviewbasic.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewConfiguration;
 
 import com.hcs.testviewbasic.R;
+import com.hcs.testviewbasic.databinding.ActivityMainBinding;
+import com.hcs.testviewbasic.utils.LogUtils;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        LogUtils.logD(TAG, "touchSlop = %d", ViewConfiguration.get(this).getScaledTouchSlop());
+        LogUtils.logD(TAG, "density = %f", getResources().getDisplayMetrics().density);
+
+        mBinding.tvv1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == mBinding.tvv1.getId()) {
+//            mBinding.tvv1.smoothScrollTo(-350, -350);
+        }
     }
 }
---
+++ chapter_3/testviewbasic/src/main/java/com/hcs/testviewbasic/view/TestScrollView.java
@@ -0,0 +1,52 @@
+package com.hcs.testviewbasic.view;
+
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+import com.hcs.testviewbasic.R;
+
+public class TestScrollView extends TextView {
+    private static final String TAG = TestScrollView.class.getSimpleName();
+    private final Animation mAnimation;
+    private final ObjectAnimator mObjectAnimator;
+    private int mLastAction;
+
+    public TestScrollView(Context context) {
+        this(context, null);
+    }
+
+    public TestScrollView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public TestScrollView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public TestScrollView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        mAnimation = AnimationUtils.loadAnimation(getContext(), R.anim.test_translate);
+        mAnimation.setFillAfter(true);
+
+        mObjectAnimator = ObjectAnimator.ofFloat(this, "translationX", 0, 100);
+        mObjectAnimator.setDuration(1000);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_UP && mLastAction == MotionEvent.ACTION_DOWN) {
+            // startAnimation(mAnimation);
+            mObjectAnimator.start();
+        }
+        mLastAction = event.getAction();
+
+        return super.onTouchEvent(event);
+    }
+}
